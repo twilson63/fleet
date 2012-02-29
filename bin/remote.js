c7fd79f9b4aaf220c33a9538823b970dc143d61f
James Halliday <mail@substack.net>
2012-02-29 00:40:50 -0800

    factored out updating the fleet.json into a separate file

--- bin/remote.js
+++ bin/remote.js
@@ -1,36 +1,23 @@
 #!/usr/bin/env node
 var argv = require('optimist').argv;
-var git = require('../lib/git');
-
 var fs = require('fs');
 var path = require('path');
 
-var gitDir = git.dir();
-var fleetFile = gitDir + '/fleet.json';
-
-var remote = {};
-if (path.existsSync(fleetFile)) {
-    remote = JSON.parse(fs.readFileSync(fleetFile));
-}
-
-function save () {
-    fs.writeFileSync(fleetFile, JSON.stringify(remote, undefined, 2) + '\n');
-}
+var git = require('../lib/git');
+var remote = require('../lib/remote');
 
 var cmd = argv._[0];
 
 if (cmd === 'add') {
     var name = argv._[1] || 'default';
-    remote[name] = { hub : argv.hub, secret : argv.secret };
-    save();
+    remote.add(name, { hub : argv.hub, secret : argv.secret });
 }
-else if (cmd === 'rm') {
+else if (cmd === 'rm' || cmd === 'remove') {
     var name = argv._[1] || 'default';
-    delete remote[name];
-    save();
+    remote.remove(name);
 }
 else if (cmd === 'ls' || cmd === 'list') {
-    console.log(JSON.stringify(remote, undefined, 2));
+    console.log(JSON.stringify(remote.list(), undefined, 2));
 }
 else {
     console.error('Usage: fleet remote (add|rm|ls)');
