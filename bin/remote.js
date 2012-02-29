4b08fce0bae3cb66d398b72bc2d50543124d00c3
James Halliday <mail@substack.net>
2012-02-28 21:24:10 -0800

    remote tracking

--- bin/remote.js
+++ bin/remote.js
@@ -1,1 +1,37 @@
-// todo: like git remote to save the --hub and --secret
+#!/usr/bin/env node
+var argv = require('optimist').argv;
+var git = require('../lib/git');
+
+var fs = require('fs');
+var path = require('path');
+
+var gitDir = git.dir();
+var fleetFile = gitDir + '/fleet.json';
+
+var remote = {};
+if (path.existsSync(fleetFile)) {
+    remote = JSON.parse(fs.readFileSync(fleetFile));
+}
+
+function save () {
+    fs.writeFileSync(fleetFile, JSON.stringify(remote, undefined, 2) + '\n');
+}
+
+var cmd = argv._[0];
+
+if (cmd === 'add') {
+    var name = argv._[1] || 'default';
+    remote[name] = { hub : argv.hub, secret : argv.secret };
+    save();
+}
+else if (cmd === 'rm') {
+    var name = argv._[1] || 'default';
+    delete remote[name];
+    save();
+}
+else if (cmd === 'ls' || cmd === 'list') {
+    console.log(JSON.stringify(remote, undefined, 2));
+}
+else {
+    console.error('Usage: fleet remote (add|rm|ls)');
+}
