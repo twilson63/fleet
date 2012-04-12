6f8cc4671eb3ceae69bee60f91fa9700454cc486
James Halliday <mail@substack.net>
2012-04-11 20:50:47 -0700

    help command summary and short error for missing topics

--- bin/help.js
+++ bin/help.js
@@ -3,4 +3,12 @@ var fs = require('fs');
 var topic = process.argv[2] || 'summary';
 var file = __dirname + '/../help/' + topic;
 
-fs.createReadStream(file).pipe(process.stdout, { end : false });
+var s = fs.createReadStream(file);
+s.pipe(process.stdout, { end : false });
+
+s.on('error', function (err) {
+    if (err.code === 'ENOENT') {
+        console.log('no help for topic ' + JSON.stringify(topic));
+    }
+    else console.error(String(err));
+});
